18101e1d06b8bd3f2a09cfe04bde41a337ef990d
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2023-08-24 23:07:32 +0200

    Desenvolvi o método do objecto 'Balancete' que calcula o SI, DIF e qtdARequisitar no load do Windows.

--- scripts/backup.js
+++ scripts/backup.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const backup = {
-    saveMainData() {
+    mainData() {
         for (let i = 0; i < mainCels.length; i++) {
             // Adicionar a lista padrão dos fármacos ao localStorage caso não esteja adicionada
             if(mainCels[i].type == "text" && !localStorage.getItem(`rb-cel${i}`)){
@@ -16,29 +16,67 @@ const backup = {
         }
     },
 
-    saveAditionalData() {
+    aditionalData() {
         for (let i = 0; i < aditionalData.length; i++) {
             aditionalData[i].addEventListener("input", () => {
                 localStorage.setItem(`rb-${aditionalData[i].id}`,`${aditionalData[i].value}`);
             }); 
             aditionalData[i].value = localStorage.getItem(`rb-${aditionalData[i].id}`);
         }
+    },
+
+    destaqueDeAutoCels() {
+        readonlyCelsDarker.addEventListener("change", ()=>{
+            readonlyCelsDarker.checked ? localStorage.setItem("rb-destaque", "on") : localStorage.removeItem("rb-destaque")
+        }
+        ),
+        localStorage.getItem("rb-destaque") && (readonlyCelsDarker.setAttribute("checked", ""),
+        menu.destacarFundoDeTotais());
+    },
+
+    tipoDeRequisicao(tipos_de_requisicao) {
+        for (let i = 0; i < tipos_de_requisicao.length; i++) {
+             // Resetar todos inputs checkbox do container 'col-tipo-de-requisicao'
+             tipos_de_requisicao[i].removeAttribute("checked");
+
+            tipos_de_requisicao.forEach(tipo => {
+                tipo.addEventListener("change", () => {
+                    localStorage.removeItem(`${tipos_de_requisicao[i].id}`);
+
+                    // Salvar o check do input activado
+                    localStorage.setItem(`${tipo.id}`, "on");
+                });
+
+                // Retornar o tipo activado
+                if(localStorage.getItem(tipo.id)) {
+                    tipo.setAttribute("checked", "");
+                }
+            });
+        }         
     }
 }
 
-let mainCels, aditionalData;
+let mainCels, aditionalData, tiposDeRequisicao, tiposDeCopiaDaFicha;
 window.addEventListener("load", () => {
 
     // INICIALIZAÏÇÃO DE VARIÁVEIS
     mainCels = document.querySelectorAll("div.body .row input");
-    aditionalData = document.querySelectorAll("div.container header input[type=text], footer input[type=text], input[type=date], input#numero-de-requisicao, input[type=checkbox]");
+    aditionalData = document.querySelectorAll("div.container > header input[type=text], header input[type=number], footer input");
+    tiposDeRequisicao = document.querySelectorAll("div.col-tipo-de-requisicao input[type=checkbox]");
+    tiposDeCopiaDaFicha = document.querySelectorAll("div.container aside input[type=checkbox]");
 
     // INVOCAÇÕES
     // Backup de fármacos da lista padrão
     if(typeof Storage != "undefined") {
-        backup.saveMainData();
-        backup.saveAditionalData();
-        menu.destacarFundoDeTotais()
+        backup.mainData();
+        backup.aditionalData();
+        backup.destaqueDeAutoCels();
+        tiposDeRequisicao.forEach( tipo => {
+            backup.tipoDeRequisicao(tiposDeRequisicao);
+        });
+        tiposDeCopiaDaFicha.forEach( tipo => {
+            backup.tipoDeRequisicao(tiposDeCopiaDaFicha);
+        });
     }
     
 });
--- scripts/balancete.js
+++ scripts/balancete.js
@@ -42,10 +42,47 @@ const balancete = {
         }
         qtdArequisitarOutput.value = quantidade_a_requisitar;
         
-    }
+    },
+
+    ifCelHasAttrData_Calc_SF_DIF_qtdAreq(cel) {
+        if(cel.dataset.stfp) {
+            cel.classList.add(`${cel.dataset.stfp}`);
+            
+            const stfp = document.querySelectorAll(`.${cel.dataset.stfp}`);
+            const stfpOutput = document.querySelector(`.${cel.dataset.stfpoutput}`);
+                  
+            let vetor_SI_E_S = (cel.dataset.stfp).split("-menos-");
+            let soma_das_saidas = document.querySelector(`.${vetor_SI_E_S[1]}`);
+
+            this.calcularStockTeorico(stfp, soma_das_saidas, stfpOutput);
+        } 
+
+        if(cel.dataset.diferenca) {
+
+            let vetor_I_e_STFP = (cel.dataset.diferenca).split("-menos-");
+
+            const inventario = document.querySelector(`.${vetor_I_e_STFP[0]}`);
+            const stfp = document.querySelector(`.${vetor_I_e_STFP[1]}`);
+            const diferencaOutput = document.querySelector(`.${cel.dataset.diferencaoutput}`);
+            
+            this.calcularDiferenca(inventario, stfp, diferencaOutput);
+        }
+
+        if(cel.dataset.qtdarequisitar) {
+
+            let vetor_S_I = (cel.dataset.qtdarequisitar).split("-menos-");
+
+            const soma_das_saidas = document.querySelector(`.${vetor_S_I[0]}`);
+            const inventario_do_stock = document.querySelector(`.${vetor_S_I[1]}`);
+            const qtdarequisitarOutput = document.querySelector(`.${cel.dataset.qtdarequisitaroutput}`);
+            
+            this.calcularQtdArequisitar(soma_das_saidas, inventario_do_stock, qtdarequisitarOutput);
+        }
+    }  
 }
 
 window.addEventListener("load", () => {
+    // Mudar a cor da página de acordo com a opção selecionada (Original, Duplicado ou Triplicado);
     const bgcTargets = document.querySelectorAll("div.container aside input");
     bgcTargets.forEach( bgcTarget => {
         bgcTarget.addEventListener("change", () => {
@@ -54,55 +91,19 @@ window.addEventListener("load", () => {
         })
     });
 
-    const tipos_de_requisicao = document.querySelectorAll("div.container div.col-tipo-de-requisicao input");
-
+    // Invocar a função "toRadio" que activa tipo de requisição, uma de cada vez
+    const tipos_de_requisicao = document.querySelectorAll("div.container div.col-tipo-de-requisicao input[type=checkbox]");
     tipos_de_requisicao.forEach( tdR => {
-        tdR.addEventListener("change", () => { balancete.toRadioCheckboxes(tipos_de_requisicao, tdR)});
+        tdR.addEventListener("change", () => balancete.toRadioCheckboxes(tipos_de_requisicao, tdR));
     });
 
     /* Calcular Stock Teórico */
-    inputCels.forEach( input => {
-
-        input.addEventListener("input", () => {
-            if(input.dataset.stfp) {
-                input.classList.add(`${input.dataset.stfp}`);
-                
-                const stfp = document.querySelectorAll(`.${input.dataset.stfp}`);
-                const stfpOutput = document.querySelector(`.${input.dataset.stfpoutput}`);
-                      
-                let vetor_SI_E_S = (input.dataset.stfp).split("-menos-");
-                let soma_das_saidas = document.querySelector(`.${vetor_SI_E_S[1]}`);
-
-                balancete.calcularStockTeorico(stfp, soma_das_saidas, stfpOutput);
-            } 
-
-            if(input.dataset.diferenca) {
-
-                let vetor_I_e_STFP = (input.dataset.diferenca).split("-menos-");
-
-                const inventario = document.querySelector(`.${vetor_I_e_STFP[0]}`);
-                const stfp = document.querySelector(`.${vetor_I_e_STFP[1]}`);
-                const diferencaOutput = document.querySelector(`.${input.dataset.diferencaoutput}`);
-                
-                balancete.calcularDiferenca(inventario, stfp, diferencaOutput);
-            }
-
-            if(input.dataset.qtdarequisitar) {
-
-                let vetor_S_I = (input.dataset.qtdarequisitar).split("-menos-");
-
-                const soma_das_saidas = document.querySelector(`.${vetor_S_I[0]}`);
-                const inventario_do_stock = document.querySelector(`.${vetor_S_I[1]}`);
-                const qtdarequisitarOutput = document.querySelector(`.${input.dataset.qtdarequisitaroutput}`);
-                
-                balancete.calcularQtdArequisitar(soma_das_saidas, inventario_do_stock, qtdarequisitarOutput);
-            }
-        }); 
+    inputCels.forEach( inputCel => {
+        inputCel.addEventListener("input", () => balancete.ifCelHasAttrData_Calc_SF_DIF_qtdAreq(inputCel)); 
+        inputCel.value !== "" && balancete.ifCelHasAttrData_Calc_SF_DIF_qtdAreq(inputCel);
     });
 
     /* TO LABEL H1 */
     const h1 = document.querySelector("div.container > header h1");
-    h1.addEventListener("click", () => {
-        document.querySelector("input#numero-de-requisicao").focus();
-    })
+    h1.addEventListener("click", () => document.querySelector("input#numero-de-requisicao").focus());
 })
