72977590beed79bb67754583cfecada72813df8f
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2024-08-21 20:11:47 -0300

    HTML/CSS/JS: Minifiquei todos os arquivos tirando os espaços entre os blocos e entre as linhas dentro de alguns blocos.

--- fisiologia/main.js
+++ fisiologia/main.js
@@ -1,22 +1,17 @@
 "use strict"
-
 const backup = {
     saveGridInputs() {
         const inputsCelulares = document.querySelectorAll(".ficha__linha-de-inputs input");
-
         for (let i = 0; i < inputsCelulares.length; i++) {
             inputsCelulares[i].addEventListener("input", () => {
                 localStorage.setItem(`${keyPrefix}-input${i}`, inputsCelulares[i].value);
             });
-        
             let inputIsSavedOnStorage = localStorage.getItem(`${keyPrefix}-input${i}`);
             if(inputIsSavedOnStorage) {
                 inputsCelulares[i].value = localStorage.getItem(`${keyPrefix}-input${i}`);
             } 
         }
-        
     },
-    
     saveExtraInputs() {
         const inputsNaoCelulares = document.querySelectorAll(".input-nao-celular");
         inputsNaoCelulares.forEach( extraInput => {
@@ -24,12 +19,9 @@ const backup = {
             extraInput.value = localStorage.getItem(`${keyPrefix}-${extraInput.id}`);
         });
     },
-
     saveCheckboxOption(checkboxes, key) {
-        
         for(const checkbox of checkboxes) {
             checkbox.addEventListener("change", () => localStorage.setItem(`${key}`, checkbox.id));
-
             let checkboxSavedOnStorage = localStorage.getItem(`${key}`);
             if(checkboxSavedOnStorage) {
                 checkbox.checked = false;
@@ -37,11 +29,9 @@ const backup = {
                     checkbox.checked = true;
                 }
             }
-            
         }
     }
 }
-
 const balancete = {
     toRadioCheckboxes(checkboxes) {
         checkboxes.forEach (checkbox => {
@@ -51,126 +41,94 @@ const balancete = {
             });
         });
     },
-
     simularDuplicadoOuTriplicado(checkbox) {
-
         const ficha = document.querySelector(".ficha");
         let copia = checkbox.dataset.copiadaficha;
-
         ficha.classList.remove("ficha--duplicado");
         ficha.classList.remove("ficha--triplicado");
         ficha.classList.add(`${copia}`);
     },
-
     filtrarEtotalizarCelulas(inputTarget) {
         if(inputTarget.dataset.stfp) {
             const classNameDosOperandos = inputTarget.dataset.stfp;
             inputTarget.classList.add(`${classNameDosOperandos}`);
             const classNameDeSomaDasSaidas = classNameDosOperandos.split("-menos-")[1];
-
             const operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
             const somaDasSaidas = document.querySelector(`.${classNameDeSomaDasSaidas}`);
             const celulaDeSaida = document.querySelector(`.${inputTarget.dataset.stfpoutput}`);
-            
-
             let soma = 0;
             for (const operando of operandos) {
                 soma+= Number(operando.value)
             }
-
             let stfp = soma - Number(somaDasSaidas.value) * 2;
             celulaDeSaida.value = stfp;
         }
-
         if(inputTarget.dataset.diferenca) {
             const classNameDosOperandos = inputTarget.dataset.diferenca;
             const classNameDeInventario = classNameDosOperandos.split("-menos-")[0];
             const classNameDeStockTeorico = classNameDosOperandos.split("-menos-")[1];
-            
-
             const inventario = document.querySelector(`.${classNameDeInventario}`);
             const stockTeorico = document.querySelector(`.${classNameDeStockTeorico}`);
             let celulaDeSaida = document.querySelector(`.${inputTarget.dataset.diferencaoutput}`);
-
             let diferenca = inventario.value - stockTeorico.value;
-
             celulaDeSaida.value = diferenca;
         }
-
         if(inputTarget.dataset.qtdarequisitar) {
             const classNameDosOperandos = inputTarget.dataset.qtdarequisitar;
             const classNameDeSomaDasSaidas = classNameDosOperandos.split("-menos-")[0];
             const classNameDeInventario = classNameDosOperandos.split("-menos-")[1];
-            
             const somaDasSaidas = document.querySelector(`.${classNameDeSomaDasSaidas}`);
             const inventario = document.querySelector(`.${classNameDeInventario}`);
             const celulaDeSaida = document.querySelector(`.${inputTarget.dataset.qtdarequisitaroutput}`);
-
             let qtdArequisitar = somaDasSaidas.value * 2 - inventario.value;
-
             if(qtdArequisitar < 0) qtdArequisitar = 0;
-            
             celulaDeSaida.value = qtdArequisitar;
         }
     },
-
     clonarHeaderDaFichaParaTodasPaginas() {
         const headerOriginal = document.querySelector(".ficha__main__header");
         const firstLinesOfEachPage = document.querySelectorAll(".ficha__linha-de-inputs--1a-da-pagina");
-
         let headersClones = document.querySelectorAll(".ficha__main__header--clone");
         for (const header of headersClones) {
             header.parentElement.removeChild(header);
         }
-
         for (let firstLine of firstLinesOfEachPage) {
             let headerClone = headerOriginal.cloneNode(!0);
             headerClone.classList.add("ficha__main__header--clone");
             firstLine.insertAdjacentElement("beforeBegin", headerClone)
         }
     },
-
     clonarFooterDaFichaParaTodasPaginas() {
         const footerOriginal = document.querySelector(".ficha__rodape");
         const headersClones = document.querySelectorAll(".ficha__main__header--clone");
-
         let footersClones = document.querySelectorAll(".ficha__rodape--clone");
         for (const footer of footersClones) {
             footer.parentElement.removeChild(footer);    
         }
-  
         for (let header of headersClones) {
             let footerClone = footerOriginal.cloneNode(!0);
             footerClone.classList.add("ficha__rodape--clone");
             header.insertAdjacentElement("beforeBegin", footerClone);
         }
-        
-
     }
 }
-
-
 function escutarEventos() { 
     // Tipo de Requisição
     const checkboxesTipoDeReq = document.querySelectorAll(".input-tipo-de-requisicao");
     balancete.toRadioCheckboxes(checkboxesTipoDeReq);
     let key = "rb-tipo-de-req"
     backup.saveCheckboxOption(checkboxesTipoDeReq, key);
-
     // Cor da Requisição 
     const checkboxesBgcModifiers = document.querySelectorAll(".checkbox-modificador-de-cor-da-ficha");
     balancete.toRadioCheckboxes(checkboxesBgcModifiers);
     key = "rb-copia-de-req"
     backup.saveCheckboxOption(checkboxesBgcModifiers, key);
-
     checkboxesBgcModifiers.forEach( checkbox => {
         checkbox.addEventListener("change", () => {
             balancete.simularDuplicadoOuTriplicado(checkbox);
         });
-
         checkbox.checked && balancete.simularDuplicadoOuTriplicado(checkbox);
     });
-   
     // Balancete propriamente dito
     const inputsCelulares = document.querySelectorAll(".ficha__linha-de-inputs input");
     inputsCelulares.forEach (inputCelular => {
@@ -178,10 +136,8 @@ function escutarEventos() {
             balancete.filtrarEtotalizarCelulas(inputCelular)
         })
         inputCelular.value !== "" && balancete.filtrarEtotalizarCelulas(inputCelular);
-    })
-
+    });
 }
-
 window.addEventListener("load", () => {
     backup.saveGridInputs();
     backup.saveExtraInputs();
--- fisiologia/menu.js
+++ fisiologia/menu.js
@@ -1,5 +1,4 @@
 "use strict"
-
 const menu = {
     realcarTotaisSe(condicao) {
         const totais = document.querySelectorAll("[readonly]");
@@ -13,37 +12,29 @@ const menu = {
             }
         }
     },
-
     filtrarFarmaco() {
         return {
             dialogBox: document.querySelector(".dialog-box-ir-para"),
             inputNumLinha: document.querySelector(".dialog-box-ir-para__input-linha"),
             numerosDeLinha: document.querySelectorAll(".ficha__num-de-linha"),
             body: document.querySelector("body"),
-
             abrirDialogBox() { 
                 menu.filtrarFarmaco().dialogBox.classList.add("--open");
                 menu.filtrarFarmaco().inputNumLinha.value = "";
                 menu.filtrarFarmaco().inputNumLinha.focus();
             },
-
             fecharDialogBox() {
                 menu.filtrarFarmaco().dialogBox.classList.remove("--open");
                 menu.filtrarFarmaco().filtrar("")
-
             },
-
             filtrar(query) {
                 let queryFormatada = formatarString(query);
                 if(queryFormatada.includes("coartem")) queryFormatada = "artemeter";
-                
                 const farmacos = document.querySelectorAll(".ficha__linha-de-inputs input:nth-child(2)");
                 for (let farmaco of farmacos) {
-                    formatarString(farmaco.value).includes(queryFormatada) ?
-                        farmaco.parentElement.classList.remove("--hide") :
-                        farmaco.parentElement.classList.add("--hide");
+                    formatarString(farmaco.value).includes(queryFormatada) ? farmaco.parentElement.classList.remove("--hide") 
+                    : farmaco.parentElement.classList.add("--hide");
                 }
-
                 let resultadosEncontrados = 0;
                 const linhasDeFarmaco = document.querySelectorAll(".ficha__linha-de-inputs");
                 for (const linha of linhasDeFarmaco) {
@@ -51,12 +42,10 @@ const menu = {
                         resultadosEncontrados++;
                     } 
                 }
-
                 resultadosEncontrados === 0 ? 
                 this.showNothingFoundMsg() : 
                 this.hideNothingFoundMsg();
             },
-
             showNothingFoundMsg() {
                 const msgNothingFound = document.querySelector(".msg-nothing-found-to-filtrar-medicamentos");
                 msgNothingFound.textContent = "Nenhum medicamento/artigo médico corresponde à pesquisa."
@@ -64,48 +53,37 @@ const menu = {
                 this.body.scrollIntoView();
                 this.body.classList.add("body--overflow-h");
             },
-
             hideNothingFoundMsg() {
                 const msgNothingFound = document.querySelector(".msg-nothing-found-to-filtrar-medicamentos");
                 msgNothingFound.classList.add("--display-none");
                 this.body.classList.remove("body--overflow-h");
             }
-           
-
-        
         }
     },
-
     esvaziarFicha() {
         return {  
             dialogBox: document.querySelector(".dialog-box-esvaziar-ficha"),
             abrirDialogBox() { 
                 const inputsDaFicha = document.querySelectorAll(".ficha__main__body input, .input-nao-celular");
-
                 let inputFilled = 0;
                 for(const input of inputsDaFicha) {
                     input.value.length > 0 && inputFilled++;
                 }
-
                 if(inputFilled === 0) {
                     const noInputFilledMsg = "A ficha já se encontra vazia."
                     alertarSobre(noInputFilledMsg);
                     return false;
                 } 
-
                 menu.esvaziarFicha().dialogBox.classList.add("--open");
                 desfoqueDoFundo("desfocar");
             },
-
             fecharDialogBox() {
                 menu.esvaziarFicha().dialogBox.classList.remove("--open");
                 desfoqueDoFundo("focar");
             },
-
             confirmar() {
                 let inputsCelulares  = document.querySelectorAll(".ficha__linha-de-inputs input");
                 const checkboxesParaInputsNaoCelulares = document.querySelectorAll("[data-for]");
-
                 let excluirMedicamentos = false;
                 for (const cb of checkboxesParaInputsNaoCelulares) {                    
                     if(cb.checked) {
@@ -118,11 +96,9 @@ const menu = {
                         }
                     }
                 }
-
                 if(!excluirMedicamentos) {
                     inputsCelulares = document.querySelectorAll(".ficha__linha-de-inputs input:nth-child(n+3)");
                 }
-
                 for (let i = 0; i < inputsCelulares.length; i++) {
                     inputsCelulares[i].value = "";
                     localStorage.removeItem(`${keyPrefix}-input${i}`);
@@ -132,33 +108,25 @@ const menu = {
             }
         }
     },
-
     imprimirFicha() {
         balancete.clonarHeaderDaFichaParaTodasPaginas();
         balancete.clonarFooterDaFichaParaTodasPaginas();
         window.print()
     },
-
     abrirArtigo(artigo) {
         const artigoSobre = document.querySelector(".artigo--sobre");
         const artigoAjuda = document.querySelector(".artigo--ajuda");
         const body = document.querySelector("body");
-
-        artigo === "sobre" ? 
-        artigoSobre.classList.add("--open") : 
-        artigoAjuda.classList.add("--open");
-
+        artigo === "sobre" ? artigoSobre.classList.add("--open") 
+        : artigoAjuda.classList.add("--open");
         body.classList.add("body--overflow-h");
         desfoqueDoFundo("desfocar");
     },
-
     fecharArtigo(artigo) {
         const artigoSobre = document.querySelector(".artigo--sobre");
         const artigoAjuda = document.querySelector(".artigo--ajuda");
         const body = document.querySelector("body");
-
         artigo === "sobre" && artigoSobre.classList.remove("--open");
-
         if(artigo === "ajuda") {
             const details = document.getElementsByTagName("details");
             for (const d of details) {
@@ -166,36 +134,29 @@ const menu = {
             }
             artigoAjuda.classList.remove("--open");
         }
-
         body.classList.remove("body--overflow-h");
         desfoqueDoFundo("focar");
     }
 }
-
 function eventos() {
     // REALCAR TOTAIS
     const checkboxRealcarTotais = document.getElementById("checkbox-realcar-totais");
     const cRt = checkboxRealcarTotais;
     cRt.addEventListener("change", () => cRt.checked ? menu.realcarTotaisSe(1) : menu.realcarTotaisSe(0));
-
     // Realcar totais no load do windows 
     if(localStorage.getItem(`${keyPrefix}-realcarTotais`)) {
         checkboxRealcarTotais.setAttribute("checked", "checked");
         menu.realcarTotaisSe(1);
     }
-
     // IR PARA LINHA
     const btnAbrirIrPara = document.querySelector(".header__menu__btn--ir-para");
     btnAbrirIrPara.addEventListener("click", menu.filtrarFarmaco().abrirDialogBox);
-
     const btnFecharIrPara = document.querySelector(".dialog-box-ir-para__btn--fechar");
     btnFecharIrPara.addEventListener("click", menu.filtrarFarmaco().fecharDialogBox);
-
     const inputNumLinha = document.querySelector(".dialog-box-ir-para__input-linha");
     inputNumLinha.addEventListener("input", () => {
         menu.filtrarFarmaco().filtrar(inputNumLinha.value)
     });
-
     // Fechar dialog-boxes-default
     const btnsFecharDialogBox = document.querySelectorAll(".dialog-box-default__btn");
     btnsFecharDialogBox.forEach( btn => {
@@ -205,35 +166,26 @@ function eventos() {
             clearInterval(btnAutoCloseLoop);
         });
     });
-
     // ESVAZIAR FICHA 
     const btnEsvaziarFicha = document.querySelector(".header__menu__btn--esvaziar-ficha");
     btnEsvaziarFicha.addEventListener("click", menu.esvaziarFicha().abrirDialogBox);
-
     const btnCancelar = document.querySelector(".dialog-box-esvaziar-ficha__btn--cancelar");
     btnCancelar.addEventListener("click", menu.esvaziarFicha().fecharDialogBox);
-
     const btnConfirmar = document.querySelector(".dialog-box-esvaziar-ficha__btn--confirmar");
     btnConfirmar.addEventListener("click", menu.esvaziarFicha().confirmar);
-
     // IMPRIMIR 
     const btnImprimir = document.querySelector(".header__menu__btn--imprimir");
     btnImprimir.addEventListener("click", menu.imprimirFicha);
-
     // Artigos
     const btnAbrirSobre = document.querySelector(".header__menu__btn--sobre");
     btnAbrirSobre.addEventListener("click", () => menu.abrirArtigo("sobre"));
-
     const btnFecharSobre = document.querySelector(".artigo__btn-x--fechar-sobre")
     btnFecharSobre.addEventListener("click", () => menu.fecharArtigo("sobre"));
-
     window.addEventListener("resize", () => {
         const artigoSobre = document.querySelector(".artigo--sobre");
-
         const itsMobile = window.innerWidth < 1024;
         const articleIsOpen = artigoSobre.matches(".--open");
         const body = document.querySelector("body");
-
         if(itsMobile && articleIsOpen) {
             desfoqueDoFundo("focar");
             location.href = `index.html#${artigoSobre.id}`;
@@ -244,20 +196,16 @@ function eventos() {
             body.classList.add("body--overflow-h");
         }       
     });
-
     const btnAbrirAjuda = document.querySelector(".header__menu__btn--ajuda");
     btnAbrirAjuda.addEventListener("click", () => menu.abrirArtigo("ajuda"));
-
     const btnFecharAjuda = document.querySelector(".artigo__btn-x--fechar-ajuda")
     btnFecharAjuda.addEventListener("click", () => menu.fecharArtigo("ajuda"));
-
     // PARTILHAR 
     const data = {
         title: "Requisição/Balancete",
         text: "Calcula automaticamente o Stock Teórico Fim do Período, Diferença entre stock teórico e stock físico e Quantidade a Requisitar com base nos dados de controlo da ficha de stock e inventário preenchidos pelo usuário. Foi desenvolvido de acordo com o modelo da ficha de requisição/balancete actualmente vigente no Serviço Nacional de Saúde em Moçambique.",
         url: "https://quinamine.github.io/requisicao-balancete/index.html"
     }
-
     const btnPartilhar = document.querySelector(".header__menu__btn--partilhar");
     btnPartilhar.addEventListener("click", () => {
         try {
@@ -266,17 +214,13 @@ function eventos() {
         } catch (e) {
             console.log("O seu navegador não tem suporte ao método 'navigator.share()'.")
         }
-    })
-
-};
-
+    });
+}
 window.addEventListener("load", eventos);
-
 window.addEventListener("keydown", event => {
-
     // CONTROL = 17 && p = 80
     if(event.ctrlKey && event.keyCode === 80) {
         event.preventDefault();
         menu.imprimirFicha();
     }
-})
+});
